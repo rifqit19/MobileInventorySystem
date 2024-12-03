229a8f3386a6b20a1a4bc3392ccc5c583bc2bad8
rifqit19 <rifqit19@gmail.com>
2024-12-03 09:28:55 +0700

    initial project

---
+++ MobileInventorySystem/MobileInventorySystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileInventorySystemApp.swift
+//  MobileInventorySystem
+//
+//  Created by rifqi triginandri on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileInventorySystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
